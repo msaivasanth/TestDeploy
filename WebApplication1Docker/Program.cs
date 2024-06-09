7399a217d9ed0ca8a83e3053b8bec9dd0a574e4a
msaivasanth <vasanths314@gmail.com>
2024-06-09 10:31:24 +0530

    Made changes for program.cs

--- WebApplication1Docker/Program.cs
+++ WebApplication1Docker/Program.cs
@@ -22,4 +22,8 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Configure Kestrel to listen on the port defined in the environment variable, default to 10000
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+app.Urls.Add($"http://*:{port}");
+
 app.Run();
