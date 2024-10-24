e4e7a72065a467c0c71fba3cbb90110cadd89044
M. Sai Vasanth <92867026+msaivasanth@users.noreply.github.com>
2024-10-24 12:49:18 +0530

    Adding few files for testing

---
+++ WebApplication1Docker/Data/DTO/UserDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace BlandAI_FetchDetails.Data.DTO
+{
+    public class UserDetails
+    {
+        public string Name { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string DateOfBirth { get; set; } = "";
+
+
+    }
+}
